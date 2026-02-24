ac88603e88567f46216bdf7c3ba784fc6f15f453
micheleferri01 <micheleferri095@gmail.com>
2026-02-24 12:57:28 +0100

    create: default layout

---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+
+
+export default function DeafaultLayout() {
+    return (
+        <>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+
+        </>
+
+    )
+}
