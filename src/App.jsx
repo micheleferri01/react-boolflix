f4d61ee311f72e8ebd797c93f50bab5fcf12467a
micheleferri01 <micheleferri095@gmail.com>
2026-02-24 15:51:29 +0100

    add: context to save the apis

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DeafaultLayout from "./layouts/DefaultLayout";
 import HomePage from "./pages/HomePage";
+import {MoviesFetchProvider} from "./contexts/MoviesFetchContext";
 
 
 export default function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route Component={DeafaultLayout}>
-            <Route index Component={HomePage}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <MoviesFetchProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route Component={DeafaultLayout}>
+              <Route index Component={HomePage} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </MoviesFetchProvider>
+
 
     </>
   )
