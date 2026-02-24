668db465a3e0c769ed4978bbe619ee40460b6da4
micheleferri01 <micheleferri095@gmail.com>
2026-02-24 12:59:03 +0100

    add: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DeafaultLayout from "./layouts/DefaultLayout";
+import HomePage from "./pages/HomePage";
 
 
 export default function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DeafaultLayout}>
+            <Route index Component={HomePage}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
