1cfe90b734a58c81e9403b45a7df6d238bbc1df3
micheleferri01 <micheleferri095@gmail.com>
2026-02-25 17:05:39 +0100

    add: responsive for cards

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -5,7 +5,7 @@ export default function CardsList({ list }) {
         <>
             {list.length != 0 ? (<>
                 <p className="text-white fs-5">Risultati trovati ({list.length})</p>
-                <div className="row row-cols-4 g-4">
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
                     {list && list.map((movie) => <Card key={movie.id} movie={movie} />)}
                 </div>
             </>) : <p className="text-white fs-5">Nessun risultato trovato</p>}
