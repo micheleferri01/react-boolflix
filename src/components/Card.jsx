1655caa3f3d05e2e964689c44445f73116d9cba3
micheleferri01 <micheleferri095@gmail.com>
2026-02-25 03:33:09 +0100

    add: conditional rendering for overview content of the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,28 @@
 import { useMoviesInfo } from "../contexts/MoviesInfoContext"
 
 export default function Card({ movie }) {
-    const { languageFlags } = useMoviesInfo();
-    
+
+    const { languageFlags, apiPoster, stars } = useMoviesInfo();
+
+    const poster = apiPoster + movie.poster_path;
+
+    console.log(movie);
+
     return (
         <>
-                <div key={movie.id} className="col">
-                    <ul>
-                        <li>Titolo: {movie.title ? movie.title : movie.name}</li>
-                        <li>Titolo originale:  {movie.original_title ? movie.original_title : movie.original_name}</li>
-                        <li>Lingua: {languageFlags(movie.original_language)} {movie.original_language}</li>
-                        <li>Voto: {movie.vote_average}</li>
+            <div className="col">
+                <div className="my-card overflow-y-auto">
+                    <img src={poster} alt={movie.title ? movie.title : movie.name} className="poster" />
+                    <ul className="px-2 py-3 movie-info list-unstyled">
+                        <li><strong>Titolo:</strong> {movie.title ? movie.title : movie.name}</li>
+                        <li><strong>Titolo originale:</strong>  {movie.original_title ? movie.original_title : movie.original_name}</li>
+                        <li><strong>Lingua:</strong> {languageFlags(movie.original_language)} {movie.original_language}</li>
+                        <li><strong>Voto:</strong> {stars(movie.vote_average)}</li>
+                        {movie.overview && <li><strong>Overview:</strong> {movie.overview}</li>}
                     </ul>
                 </div>
+
+            </div>
         </>
     )
 }
